ed2b2cc33bf2a6ae8eef8ce3b90fcef20232b35a
Siya <siajabbarova@gmail.com>
2024-02-14 16:40:14 +0400

    final commit

--- scripts/gameZone.js
+++ scripts/gameZone.js
@@ -1,3 +1,27 @@
+class Character{
+    constructor(name, health, attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+}
+
+class Enemy extends Character{
+    constructor(name, health, attackPower) {
+        super(name, health, attackPower);
+    }
+}
+
+class Player extends Character{
+    constructor(name, health, attackPower) {
+        super(name, health, attackPower);
+    }
+}
+
+let enemy = new Enemy("Enemy", 100, 18);
+let player = new Player("Player", 100, 7);
+let click = Math.floor(Math.random() * 5);
+
 document.addEventListener("DOMContentLoaded", function() {
     var imgSrc = localStorage.getItem("characterImageSrc");
     if (imgSrc) {
@@ -8,3 +32,30 @@ document.addEventListener("DOMContentLoaded", function() {
     }}
 }
 );
+
+document.getElementById("attackBtn").addEventListener("click", function(){
+    document.getElementById("enemyHealth").value -=player.attackPower;
+    if(click == 0 || click < 1){
+        document.getElementById("playerHealth").value -=enemy.attackPower;
+        click = Math.floor(Math.random() * 5);
+    }
+    click--;
+    if(document.getElementById("playerHealth").value <= 0 || document.getElementById("enemyHealth").value <= 0){
+        const innerDiv = document.getElementById("gamezone-body");
+        if(document.getElementById("playerHealth").value <= 0){
+            innerDiv.style.marginTop = "270px"
+            innerDiv.innerHTML = "You Lost!";
+            innerDiv.style.color = "red";
+            innerDiv.style.fontSize = "50px";
+            console.log("You Lost!");
+
+        }
+        else{
+            innerDiv.style.marginTop = "270px"
+            innerDiv.innerHTML = "You Won!";
+            innerDiv.style.color = "green";
+            innerDiv.style.fontSize = "50px";
+            console.log("You Won!");
+        }
+    }
+})
--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,3 @@
-class Character{
-    constructor(name, health, attackPower) {
-        this.name = name;
-        this.health = health;
-        this.attackPower = attackPower;
-    }
-}
-
-class Enemy extends Character{
-    constructor(name, health, attackPower) {
-        super(name, health, attackPower);
-    }
-}
-
-class Player extends Character{
-    constructor(name, health, attackPower) {
-        super(name, health, attackPower);
-    }
-}
 
 let characterSrc;
 let gameZoneBody;
